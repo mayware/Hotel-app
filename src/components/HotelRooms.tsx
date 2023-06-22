7784084065a4aa3703194b4b4338c30a6d420310
tofa93 <tofig.valiyev1993@gmail.com>
2023-06-22 03:30:23 +0200

    Added new features

--- src/components/HotelRooms.tsx
+++ src/components/HotelRooms.tsx
@@ -5,18 +5,26 @@ interface HotelRooms {
     hotelId: number;
 }
 
-export interface Room {
-    id: number;
-    name: string;
-}
-
 export interface Rooms {
     id: number;
     name: string;
     rooms: Room[];
 }
 
+export interface Room {
+    id: number;
+    name: string;
+    longDescription: string;
+    occupancy: Occupancy;
+}
+
+export interface Occupancy {
+    maxAdults: number;
+    maxChildren: number;
+}
+
 const HotelRooms = ({ hotelId }: HotelRooms) => {
+    console.log(hotelId);
     const { data: rooms, error, isPending } = useFetch(`https://obmng.dbm.guestline.net/api/roomRates/OBMNG/${hotelId}`)
 
     return (
